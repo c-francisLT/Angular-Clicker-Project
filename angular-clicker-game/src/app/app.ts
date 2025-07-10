e0204cfb17f7d898a6bbf086444901292ce91e5d
Curt Francis <cfrancis@leantechniques.com>
2025-07-10 14:51:22 -0400

    Made a clicker component

--- angular-clicker-game/src/app/app.ts
+++ angular-clicker-game/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Clicker } from './clicker.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Clicker],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ angular-clicker-game/src/app/clicker.component.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from "@angular/core";
+
+@Component({
+    selector: 'clicker-check',
+    template: `<div> Click </div>`
+})
+
+export class Clicker{
+    onClick = () =>{
+        //Fill when done with enemy
+    }
+}
